b0b748631fa99820d5a97a55227f59789cb6b8b4
clarkccc <yu900618@126.com>
2016-05-24 17:21:11 +0800

    增加DelayQueue实现的TimerTask实例

--- java/lang/concurrent/collection/DelayQueueTest.java
+++ java/lang/concurrent/collection/DelayQueueTest.java
@@ -1,5 +1,6 @@
 package lang.concurrent.collection;
 
+import java.util.Date;
 import java.util.Timer;
 import java.util.concurrent.DelayQueue;
 import java.util.concurrent.Delayed;
@@ -8,28 +9,70 @@ import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * Created by yuzhm on 2016/5/24.
+ * 用DelayQueue实现循环的TimerTask
  */
+
 public class DelayQueueTest {
+    private static  final DelayQueue<TimeTask> buffer=new DelayQueue<TimeTask>();
     public static void main(String arg[]){
-        DelayQueue<Delayed> buffer=new DelayQueue<Delayed>();
+        buffer.add(new TimeTask(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("3:"+new Date().toString());
+            }
+        },3,TimeUnit.SECONDS));
+        buffer.add(new TimeTask(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("5:"+new Date().toString());
+            }
+        },5,TimeUnit.SECONDS));
+        startComsumerThread();
     }
-    public static void startProducerThread(final DelayQueue<Delayed> buffer){
+
+    public static class TimeTask implements Delayed{
+        Runnable task;
+        final long time;
+        final TimeUnit unit;
+        long timeStart;
+
+        public TimeTask(Runnable task, long time, TimeUnit unit) {
+            this.task = task;
+            this.time = time;
+            this.unit = unit;
+            timeStart = System.currentTimeMillis()+unit.toMillis(time);
+        }
+
+        @Override
+        public long getDelay(TimeUnit unit) {
+            return unit.convert(timeStart-System.currentTimeMillis(),TimeUnit.MILLISECONDS);
+        }
+
+        @Override
+        public int compareTo(Delayed o) {
+            return Long.compare(getDelay(TimeUnit.MILLISECONDS),o.getDelay(TimeUnit.MILLISECONDS));
+        }
+
+        @Override
+        protected Object clone() {
+            return new TimeTask(task,time,unit);
+        }
+    }
+
+    public static void startComsumerThread(){
         new Thread(new Runnable() {
             @Override
             public void run() {
-                buffer.add(new Delayed() {
-                    private long init=System.currentTimeMillis();
-                    @Override
-                    public long getDelay(TimeUnit unit) {
-                        return init;
+                while(true){
+                    try {
+                        TimeTask take = buffer.take();
+                        new Thread(take.task).start();
+                        buffer.add((TimeTask)(take.clone()));
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
                     }
-                    @Override
-                    public int compareTo(Delayed o) {
-                        return 1;
-                    }
-                });
+                }
             }
         }).start();
     }
-    public static void startComsumerThread(DelayQueue<Delayed> buffer){}
 }
