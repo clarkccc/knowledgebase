7e4606f5b7a3b9f4d3052fe061da9a24ace88f3d
clarkccc <yu900618@126.com>
2016-05-27 14:11:49 +0800

    用新的线程池配置方式

--- java/lang/concurrent/data/element/ThreadLocalInExecutorTest.java
+++ java/lang/concurrent/data/element/ThreadLocalInExecutorTest.java
@@ -1,5 +1,7 @@
 package lang.concurrent.data.element;
 
+import lang.concurrent.trick.ThreadUtil;
+
 import java.util.Date;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -8,18 +10,17 @@ import java.util.concurrent.Executors;
  * Created by yuzhm on 2016/5/25.
  * 查看线程池条件下使用ThreadLocal,必须初始化或清空,以免串话
  */
-//todo
+
 public class ThreadLocalInExecutorTest {
 
     public static void main(String arg[]) throws InterruptedException {
+        ThreadUtil.ServiceBuild build = new ThreadUtil.ServiceBuild();
+        build=build.withExecutor(Executors.newFixedThreadPool(3));
         for (int i = 0; i < 10; i++) {
-            addAThread(new ReanAble());
+            build.addTask(new ReanAble());
         }
+        build.build();
     }
-
-    private static ExecutorService service= Executors.newFixedThreadPool(3);
-
-    public static void addAThread(Runnable runnable){ service.submit(runnable); }
 
     public static class ReanAble implements Runnable{
         private static ThreadLocal<String> data=new ThreadLocal<String>();
@@ -36,4 +37,5 @@ public class ThreadLocalInExecutorTest {
             data.set(current);
         }
     }
+
 }
